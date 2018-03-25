f53050880c96309da981f21135a2089ea5ef9622
9Andreyka <andystory@mail.ru>
2018-03-25 20:53:54 +0700

    fix last commit

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -15,6 +15,7 @@ use app\models\App\Task;
 use app\services\TaskManageService;
 use SebastianBergmann\Timer\RuntimeException;
 use Yii;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 
 class TaskController extends Controller
